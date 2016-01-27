18569abb42d45da8aef71c1529a7ab2d45f620be
Mikleane Chouinard <mikleane@msn.com>
2016-01-26 18:41:27 -0800

    final commit06

--- class-06-ajax-and-json/pair-assignment/mikleane-justin06/scripts/article.js
+++ class-06-ajax-and-json/pair-assignment/mikleane-justin06/scripts/article.js
@@ -38,8 +38,8 @@ Article.loadAll = function(rawData) {
 
   rawData.forEach(function(ele) {
     Article.all.push(new Article(ele));
-  })
-}
+  });
+};
 
 // This function will retrieve the data from either a local or remote source,
 // and process it, then hand off control to the View.
@@ -49,10 +49,8 @@ Article.fetchAll = function() {
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
         //TODO: What do we pass in here to the .loadAll function?
+    console.log('hello first');
     Article.loadAll(JSON.parse(localStorage.rawData));
-
-
-
   // );
 //TODO: What method do we call to render the index page?
     articleView.initIndexPage();
@@ -62,11 +60,10 @@ Article.fetchAll = function() {
     // cache it in localStorage so we can skip the server call next time,
     // then load all the data into Article.all with the .loadAll function above,
     // and then render the index page.
-    var url = '../data/hackerIpsum.json';
-    $.getJSON(url, function(rawData){
+    $.getJSON('data/hackerIpsum.json',function(rawData) {
+      console.log('hello');
       Article.loadAll(rawData);
       localStorage.rawData = JSON.stringify(rawData);
-      localStorage.rawData = JSON.parse(rawData);
       articleView.initIndexPage();
 
     });
